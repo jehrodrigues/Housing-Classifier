4c22b5d15c698a1ee4ad3370a509f364f232cbee
Jessica Silva <jsc.rodrigues@gmail.com>
2022-07-12 20:49:43 +0100

    Housing Classifier with Transformers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     version='0.1',
-    description="Hate Speech Detection with Transformers.",
+    description="Housing Classifier with Transformers.",
     author='Jessica Silva',
     packages=find_packages(),
 )
